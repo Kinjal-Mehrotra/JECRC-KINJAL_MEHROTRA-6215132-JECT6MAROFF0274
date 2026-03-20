aec6320cbb2e5a0adddd95ae3fdae66f8fce61c3
Kinjal-Mehrotra <kinjalmehrotra4@gmail.com>
2026-03-20 10:39:29 +0530

    Changes to Multi_select file

--- 19-March-2026/Multi_select.py
+++ 19-March-2026/Multi_select.py
@@ -1,21 +1,29 @@
-
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
-#you are not supposed to use implicit and explicit waits together
+opts=webdriver.ChromeOptions()
+opts.add_experimental_option('detach',True)
 
-driver=webdriver.Chrome()
-driver.get('https://demo.mobiscroll.com/select/multiple-select')
+driver=webdriver.Chrome(options=opts)
+
+driver.get('https://testautomationpractice.blogspot.com/')
 driver.maximize_window()
 
-multi_drop=driver.find_element(By.XPATH,'//select[@id="multiple-select-select"]')
+multi_drop=driver.find_element(By.ID,'colors')
 select=Select(multi_drop)
 
-if select.is_multiple():
-    select.select_by
+if select.is_multiple:
+    select.select_by_value('blue')
+    select.select_by_index(5)
+    select.select_by_visible_text('Yellow')
+
+list_of_colors=[i.text for i in select.all_selected_options]
+print(list_of_colors)
+select.deselect_by_value('blue')
+list_of_colors_2=[i.text for i in select.all_selected_options]
+print(list_of_colors_2)
